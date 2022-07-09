27742e8f183cbe2e7ed2068f4bdf8baa86c0b785
EAGLE5642 <65916986+EAGLE5642@users.noreply.github.com>
2022-07-10 07:43:02 +0900

    Update JS.js

--- JavaScript/JS.js
+++ JavaScript/JS.js
@@ -1,20 +1,29 @@
-
 function main(){
 	document.write("おかりなさい<br>");
 
 	document.bgColor="#ffc0cb";
 }
 
+//スケジュール管理ソフト起動
 function tap_schedule(){
 	alert("スケジュール管理ボタンを押しました");
 }
 
+//Google検索
 function tap_search(){
 	//テキストフィールドの文字取得
 	word = document.getElementById("text1").value;
 
-	//Google検索
+	//検索
 	window.location.href="https://google.com/search?q="+word;
+}
+
+function tap_site(){
+	if(document.getElementById("site1")){
+		site=document.getElementById("site1").value;
+
+		window.location.href="https://google.com/";
+	}
 }
 
 main();
