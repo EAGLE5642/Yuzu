041189741c7cfd01f430c38594719e6da0b5446d
EAGLE5642 <65916986+EAGLE5642@users.noreply.github.com>
2022-07-10 07:20:29 +0900

    Add files via upload

---
+++ JavaScript/JS.js
@@ -0,0 +1,22 @@
+
+function main(){
+	document.write("おかりなさい<br>");
+
+	document.write("ゆずさん！！".fontsize(6));
+
+	document.bgColor="#ffc0cb";
+}
+
+function tap_schedule(){
+	alert("スケジュール管理ボタンを押しました");
+}
+
+function tap_search(){
+	//テキストフィールドの文字取得
+	word = document.getElementById("text1").value;
+
+	//Google検索
+	window.location.href="https://google.com/search?q="+word;
+}
+
+main();
