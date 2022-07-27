2bae9b7c0a9e6d2a3c38a388575d55efe17ceb18
EAGLE5642 <65916986+EAGLE5642@users.noreply.github.com>
2022-07-28 00:41:56 +0900

    update(2022/7/28)

--- JavaScript/schedule.js
+++ JavaScript/schedule.js
@@ -1,75 +1,18 @@
-const week = ["日", "月", "火", "水", "木", "金", "土"];
-const today = new Date();
-// 月末だとずれる可能性があるため、1日固定で取得
-var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
+document.addEventListener('DOMContentLoaded',function(){
+	const tabs=document.getElementsByClassName('tab');
 
-// 初期表示
-window.onload = function () {
-	document.bgColor="#ffc0cb";
-    showProcess(today, calendar);
-};
-// 前の月表示
-function prev(){
-    showDate.setMonth(showDate.getMonth() - 1);
-    showProcess(showDate);
-}
+	for(let i=0;i<tabs.length;i++){
+		tabs[i].addEventListener('click',tabSwitch,false);
+	}
 
-// 次の月表示
-function next(){
-    showDate.setMonth(showDate.getMonth() + 1);
-    showProcess(showDate);
-}
+	function tabSwitch(){
+		document.getElementsByClassName('is-active')[0].classList.remove('is-active');
+		this.classList.add('is-active');
 
-// カレンダー表示
-function showProcess(date) {
-    var year = date.getFullYear();
-    var month = date.getMonth();
-    document.querySelector('#header').innerHTML = year + "年 " + (month + 1) + "月";
+		document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+		const arrayTabs=Array.prototype.slice.call(tabs);
+		const index=arrayTabs.indexOf(this);
 
-    var calendar = createProcess(year, month);
-    document.querySelector('#calendar').innerHTML = calendar;
-}
-
-// カレンダー作成
-function createProcess(year, month) {
-    // 曜日
-    var calendar = "<table><tr class='dayOfWeek'>";
-    for (var i = 0; i < week.length; i++) {
-        calendar += "<th>" + week[i] + "</th>";
-    }
-    calendar += "</tr>";
-
-    var count = 0;
-    var startDayOfWeek = new Date(year, month, 1).getDay();
-    var endDate = new Date(year, month + 1, 0).getDate();
-    var lastMonthEndDate = new Date(year, month, 0).getDate();
-    var row = Math.ceil((startDayOfWeek + endDate) / week.length);
-
-    // 1行ずつ設定
-    for (var i = 0; i < row; i++) {
-        calendar += "<tr>";
-        // 1colum単位で設定
-        for (var j = 0; j < week.length; j++) {
-            if (i == 0 && j < startDayOfWeek) {
-                // 1行目で1日まで先月の日付を設定
-                calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
-            } else if (count >= endDate) {
-                // 最終行で最終日以降、翌月の日付を設定
-                count++;
-                calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
-            } else {
-                // 当月の日付を曜日に照らし合わせて設定
-                count++;
-                if(year == today.getFullYear()
-                  && month == (today.getMonth())
-                  && count == today.getDate()){
-                    calendar += "<td class='today'>" + count + "</td>";
-                } else {
-                    calendar += "<td>" + count + "</td>";
-                }
-            }
-        }
-        calendar += "</tr>";
-    }
-    return calendar;
-}
+		document.getElementsByClassName('panel')[index].classList.add('is-show');
+	};
+},false);
